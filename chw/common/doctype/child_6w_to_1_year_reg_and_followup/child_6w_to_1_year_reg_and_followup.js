b9bcef2f5287531b8dfb4131e4ba1d1580120472
Sharmilaapf <sharmila.m@azimpremjifoundation.org>
2026-09-22 17:32:14 +0530

    add: forms

---
+++ chw/common/doctype/child_6w_to_1_year_reg_and_followup/child_6w_to_1_year_reg_and_followup.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, tech4socialsector@azimpremjifoundation.org and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Child 6w to 1 Year Reg and Followup", {
+// 	refresh(frm) {
+
+// 	},
+// });
